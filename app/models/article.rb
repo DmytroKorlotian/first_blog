e7b3795e9638686d50c9a57081a52f599a535c21
Dmytro Korlotian <dima@korlotian.pl>
2016-07-22 21:57:34 +0200

    add comments

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,8 +1,10 @@
 class Article < ApplicationRecord
   validates :title, presence: true,
                     length: { minimum: 5 }
-
-
+  
   validates :text, presence: true,
 					 length: { minimum: 20 }
+
+  has_many :comments
+
 end
